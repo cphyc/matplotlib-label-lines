7ce5e5714e53fbe6c31a9580c28b7247d219ca2e
Corentin Cadiou <contact@cphyc.me>
2018-06-28 15:43:14 +0200

    More tests

--- labellines/test.py
+++ labellines/test.py
@@ -17,12 +17,10 @@ def test_linspace():
     labelLines(plt.gca().get_lines(), zorder=2.5)
     plt.xlabel('$x$')
     plt.ylabel('$f(x)$')
-    plt.savefig('linear.pdf')
 
-
-@image_comparison(baseline_images=['labels_log'],
+@image_comparison(baseline_images=['labels_ylog'],
                   extensions=['png'])
-def test_logspace():
+def test_ylogspace():
     plt.clf()
     x = np.linspace(0, 1)
     K = [1, 2, 4]
@@ -30,9 +28,39 @@ def test_logspace():
     for k in K:
         plt.plot(x, np.exp(k * x), label='$f(x)=exp(%s x)$' % k)
 
-    labelLines(plt.gca().get_lines(), zorder=2.5)
     plt.yscale('log')
+    labelLines(plt.gca().get_lines(), zorder=2.5)
     plt.xlabel('$x$')
     plt.ylabel('$f(x)$')
 
-    plt.savefig('log.pdf')
+@image_comparison(baseline_images=['labels_xlog'],
+                  extensions=['png'])
+def test_xlogspace():
+    plt.clf()
+    x = np.linspace(0, 1)
+    K = [1, 2, 4]
+
+    for k in K:
+        plt.plot(10**x, k*x, label='$f(x)=%s x$' % k)
+
+    plt.xscale('log')
+    labelLines(plt.gca().get_lines(), zorder=2.5)
+    plt.xlabel('$x$')
+    plt.ylabel('$f(x)$')
+
+
+@image_comparison(baseline_images=['labels_xylog'],
+                  extensions=['png'])
+def test_xylogspace():
+    plt.clf()
+    x = np.geomspace(1e-1, 1e1)
+    K = np.arange(-5, 5, 2)
+
+    for k in K:
+        plt.plot(x, x**k, label='$f(x)=x^{%s}$' % k)
+
+    plt.xscale('log')
+    plt.yscale('log')
+    labelLines(plt.gca().get_lines(), zorder=2.5)
+    plt.xlabel('$x$')
+    plt.ylabel('$f(x)$')
