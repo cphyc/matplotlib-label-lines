81251a1e8951fbedf0066db450f20534fcc9f6ec
Corentin Cadiou <contact@cphyc.me>
2019-05-01 18:55:27 +0200

    update test to pytest

--- labellines/test.py
+++ labellines/test.py
@@ -1,94 +1,90 @@
-import matplotlib as mpl
-mpl.use('Agg')
-
 from labellines import labelLines, labelLine
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.testing.decorators import image_comparison
 from numpy.testing import assert_raises
 
+import pytest
 
-@image_comparison(baseline_images=['labels_linear'],
-                  extensions=['png'])
+@pytest.mark.mpl_image_compare
 def test_linspace():
     plt.clf()
     x = np.linspace(0, 1)
     K = [1, 2, 4]
 
     for k in K:
-        plt.plot(x, np.sin(k * x), label='$f(x)=sin(%s x)$' % k)
+        plt.plot(x, np.sin(k * x), label=r'$f(x)=\sin(%s x)$' % k)
 
     labelLines(plt.gca().get_lines(), zorder=2.5)
     plt.xlabel('$x$')
     plt.ylabel('$f(x)$')
+    return plt.gcf()
 
-@image_comparison(baseline_images=['labels_ylog'],
-                  extensions=['png'])
+@pytest.mark.mpl_image_compare
 def test_ylogspace():
     plt.clf()
     x = np.linspace(0, 1)
     K = [1, 2, 4]
 
     for k in K:
-        plt.plot(x, np.exp(k * x), label='$f(x)=exp(%s x)$' % k)
+        plt.plot(x, np.exp(k * x), label=r'$f(x)=\exp(%s x)$' % k)
 
     plt.yscale('log')
     labelLines(plt.gca().get_lines(), zorder=2.5)
     plt.xlabel('$x$')
     plt.ylabel('$f(x)$')
+    return plt.gcf()
 
-@image_comparison(baseline_images=['labels_xlog'],
-                  extensions=['png'])
+@pytest.mark.mpl_image_compare
 def test_xlogspace():
     plt.clf()
     x = np.linspace(0, 1)
     K = [1, 2, 4]
 
     for k in K:
-        plt.plot(10**x, k*x, label='$f(x)=%s x$' % k)
+        plt.plot(10**x, k*x, label=r'$f(x)=%s x$' % k)
 
     plt.xscale('log')
     labelLines(plt.gca().get_lines(), zorder=2.5)
     plt.xlabel('$x$')
     plt.ylabel('$f(x)$')
+    return plt.gcf()
 
-
-@image_comparison(baseline_images=['labels_xylog'],
-                  extensions=['png'])
+@pytest.mark.mpl_image_compare
 def test_xylogspace():
     plt.clf()
     x = np.geomspace(1e-1, 1e1)
     K = np.arange(-5, 5, 2)
 
     for k in K:
-        plt.plot(x, x**k, label='$f(x)=x^{%s}$' % k)
+        plt.plot(x, x**k, label=r'$f(x)=x^{%s}$' % k)
 
     plt.xscale('log')
     plt.yscale('log')
     labelLines(plt.gca().get_lines(), zorder=2.5)
     plt.xlabel('$x$')
     plt.ylabel('$f(x)$')
+    return plt.gcf()
 
 
-@image_comparison(baseline_images=['labels_align'],
-                  extensions=['png'])
+@pytest.mark.mpl_image_compare
 def test_align():
     x = np.linspace(0, 2*np.pi)
     y = np.sin(x)
 
-    lines = plt.plot(x, y, label='$sin(x)$')
+    lines = plt.plot(x, y, label=r'$\sin(x)$')
 
     labelLines(lines, align=False)
+    return plt.gcf()
 
-@image_comparison(baseline_images=['labels_range'],
-                  extensions=['png'])
+@pytest.mark.mpl_image_compare
 def test_labels_range():
     x = np.linspace(0, 1)
 
-    plt.plot(x, np.sin(x), label='$\sin x$')
-    plt.plot(x, np.cos(x), label='$\cos x$')
+    plt.plot(x, np.sin(x), label=r'$\sin x$')
+    plt.plot(x, np.cos(x), label=r'$\cos x$')
 
     labelLines(plt.gca().get_lines(), xvals=(0, .5))
+    return plt.gcf()
 
 def test_label_range():
     x = np.linspace(0, 1)
