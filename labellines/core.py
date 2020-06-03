8fd4df40794fb59ea029218b7cbbdc9ae0a1ff07
Max Ghenis <mghenis@gmail.com>
2020-06-03 15:31:15 -0700

    Make shrink_factor a parameter to labelLines()

--- labellines/core.py
+++ labellines/core.py
@@ -97,7 +97,7 @@ v        return date2num(x) if isinstance(x, datetime) else x
     ax.text(x, y, label, rotation=rotation, **kwargs)
 
 
-def labelLines(lines, align=True, xvals=None, drop_label=False, **kwargs):
+def labelLines(lines, align=True, xvals=None, drop_label=False, shrink_factor=0.05, **kwargs):
     '''Label all lines with their respective legends.
 
     Parameters
@@ -113,6 +113,8 @@ def labelLines(lines, align=True, xvals=None, drop_label=False, **kwargs):
     drop_label : bool, optional
        If True, the label is consumed by the function so that subsequent calls to e.g. legend
        do not use it anymore.
+    shrink_factor : double, optional
+       Relative distance from the edges to place closest labels. Defaults to 0.05.
     kwargs : dict, optional
        Optional arguments passed to ax.text
     '''
@@ -137,9 +139,8 @@ def labelLines(lines, align=True, xvals=None, drop_label=False, **kwargs):
 
     if xvals is None:
         xvals = ax.get_xlim()  # set axis limits as annotation limits, xvals now a tuple
-        SHRINK_FACTOR = 0.05  # How far out (relatively) from the edges to place closest labels.
         xvals_rng = xvals[1] - xvals[0]
-        shrinkage = xvals_rng * SHRINK_FACTOR
+        shrinkage = xvals_rng * shrink_factor
         xvals = (xvals[0] + shrinkage, xvals[1] - shrinkage)
     if type(xvals) == tuple:
         xmin, xmax = xvals
