22e9fa8e78b173edfc77a14113fb1c83f9cbd5e2
Corentin Cadiou <corentin.cadiou@cphyc.me>
2021-07-09 14:18:27 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("LICENSE") as f:
 
 setup(
     name="matplotlib-label-lines",
-    version="0.3.9",
+    version="0.4.0",
     description="Label lines in matplotlib.",
     long_description=long_description,
     long_description_content_type="text/markdown",
