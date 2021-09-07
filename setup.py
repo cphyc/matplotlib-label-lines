75f9b94457682804f440919f1f4bafcd7baf30b7
Corentin Cadiou <corentin.cadiou@cphyc.me>
2021-09-07 11:57:16 +0100

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-with open("Readme.md") as file:
-    long_description = file.read()
-
-with open("LICENSE") as f:
-    license = f.read()
-
-setup(
-    name="matplotlib-label-lines",
-    version="0.4.0",
-    description="Label lines in matplotlib.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Corentin Cadiou",
-    author_email="contact@cphyc.me",
-    url="https://github.com/cphyc/matplotlib-label-lines",
-    license="MIT",
-    packages=find_packages(exclude=("tests", "docs")),
-    install_requires=["numpy", "matplotlib"],
-    include_package_data=True,
-)
+setup()
